20ec0516171fb85bb37bd2bec43edd7427b90f8f
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2023-12-06 23:54:07 +0530

    Update cookieclicker.py

--- CookieClicker/cookieclicker.py
+++ CookieClicker/cookieclicker.py
@@ -33,19 +33,19 @@ while True:
 )
     cookies_click = driver.find_element(By.ID,"bigCookie")
     cookies_click.click()
-    cookies_count = int(driver.find_element(By.ID,"cookies").text.split(" ")[0])
-    cursor_clicker = int(driver.find_element(By.ID,"productPrice0").text)
+    cookies_count = eval(driver.find_element(By.ID,"cookies").text.split(" ")[0])
+    cursor_clicker = eval(driver.find_element(By.ID,"productPrice0").text)
     buy_cursor =  driver.find_element(By.ID,"product0")
     if cookies_count>=cursor_clicker:
         buy_cursor.click()
 
-    gandma_clicker = int(driver.find_element(By.ID,"productPrice1").text)
+    gandma_clicker = eval(driver.find_element(By.ID,"productPrice1").text)
     buy_grandma =  driver.find_element(By.ID,"product1")
     if cookies_count>= gandma_clicker:
         buy_grandma.click()
 
     try:
-        farm_clicker = int(driver.find_element(By.ID,"productPrice2").text)
+        farm_clicker = eval(driver.find_element(By.ID,"productPrice2").text)
         buy_farm =  driver.find_element(By.ID,"product2")
         if cookies_count>= farm_clicker:
             buy_farm.click()
@@ -53,7 +53,7 @@ while True:
         continue
 
     try:    
-        Mine_clicker = int(driver.find_element(By.ID,"productPrice1").text)
+        Mine_clicker = eval(driver.find_element(By.ID,"productPrice1").text)
         buy_Mine =  driver.find_element(By.ID,"product1")
         if cookies_count>= Mine_clicker:
             buy_Mine.click()
@@ -61,7 +61,7 @@ while True:
         continue
 
     try:
-        Factory_clicker = int(driver.find_element(By.ID,"productPrice1").text)
+        Factory_clicker = eval(driver.find_element(By.ID,"productPrice1").text)
         buy_Factory =  driver.find_element(By.ID,"product1")
         if cookies_count>= Factory_clicker:
             buy_Factory.click()   
